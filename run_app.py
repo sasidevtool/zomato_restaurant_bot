88c9f89e3bb7a8a29d9f020bd0629c683baf4292
Sasidhar Attaluri <attaluri@adobe.com>
2018-08-19 13:23:26 +0530

    Removing tokens

--- run_app.py
+++ run_app.py
@@ -7,11 +7,5 @@ from rasa_slack_connector import SlackInput
 nlu_interpreter = RasaNLUInterpreter('./models/nlu/default/restaurantnlu')
 agent = Agent.load('./models/dialogue', interpreter = nlu_interpreter)
 
-input_channel = SlackInput('xoxp-412623938595-413793902855-416392787079-ede4781c414d804788e6321f1ef04979', #app verification token
-							'xoxb-412623938595-413934206631-8YxnNnkgth6fouCemve8920S', # bot verification token
-							'aeMa8f0hOBtDbuR0aPQMTBac', # slack verification token
-							True)
-
-#input_channel = SlackInput(slack_token='xoxp-412623938595-413793902855-416392787079-ede4781c414d804788e6321f1ef04979')
 
 agent.handle_channel(HttpInputChannel(5004, '/', input_channel))
